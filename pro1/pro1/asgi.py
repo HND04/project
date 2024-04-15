a25568ba11ac1b001490eb000fe3a6e1bc46301b
nd <225004051@sastra.ac.in>
2024-04-16 01:21:36 +0530

    first commit

--- pro1/pro1/asgi.py
+++ pro1/pro1/asgi.py
@@ -4,6 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from pro1 import routing
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
 
 application = ProtocolTypeRouter({
